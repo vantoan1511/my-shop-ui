ef02ab3d776c3ff7cc5e02e34d725753013d9868
Toan Nguyen <vantoan1517@gmail.com>
2024-06-29 12:44:58 +0700

    Update to show list of products

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {ProductCardComponent} from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
     imports: [
-        HeaderComponent
+        HeaderComponent,
+        ProductCardComponent
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
