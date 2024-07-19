77407159e1522b60b2a7d84970811ccf66a9b381
Toan Nguyen <vantoan1517@gmail.com>
2024-07-19 23:47:41 +0700

    Update guards for admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,22 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ProductDetailsComponent} from "./product-details/product-details.component";
 import {authGuard} from "./auth.guard";
+import {UsersComponent} from "./users/users.component";
 
 export const routes: Routes = [
     {
         path: '', component: HomeComponent,
     },
     {
-        path: 'products/:id', component: ProductDetailsComponent, canActivate: [authGuard],
-    }
+        path: 'products/:id',
+        component: ProductDetailsComponent,
+        canActivate: [authGuard],
+        data: {expectedRoles: ['admin']}
+    },
+    {
+        path: 'users',
+        component: UsersComponent,
+        canActivate: [authGuard],
+        data: {expectedRoles: ['admin']}
+    },
 ];
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,28 @@
-import {CanActivateFn} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {KeycloakService} from "keycloak-angular";
 import {inject} from "@angular/core";
 
 export const authGuard: CanActivateFn = (route, state) => {
     const keycloakService = inject(KeycloakService);
+    const router = inject(Router);
+    const expectedRoles: string[] = route.data['expectedRoles'];
+    const currentRoles: string[] = keycloakService.getUserRoles();
+
+    console.log('Expected roles: ', expectedRoles);
+    console.log('Current roles: ', currentRoles);
+
     if (!keycloakService.isLoggedIn()) {
         keycloakService.login();
     }
-    return keycloakService.isLoggedIn();
+
+    // if (!expectedRoles || expectedRoles.length === 0) {
+    //     return true;
+    // }
+
+    if (!expectedRoles.every(role => currentRoles.includes(role.toUpperCase()))) {
+        router.navigate(['']);
+        return false;
+    }
+
+    return true;
 };
