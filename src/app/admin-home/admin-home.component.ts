60fcf64d114e52163065b5eadce8cb9ca5b1258c
Toan Nguyen <vantoan1517@gmail.com>
2024-07-20 13:24:06 +0700

    Update admin home page

--- src/app/admin-home/admin-home.component.ts
+++ src/app/admin-home/admin-home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {AdminHeaderComponent} from "../admin-header/admin-header.component";
 
 @Component({
   selector: 'app-admin-home',
   standalone: true,
     imports: [
-        RouterOutlet
+        RouterOutlet,
+        AdminHeaderComponent
     ],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.scss'
