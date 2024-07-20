3374e95f5df3738804d99e23af2e552003ac61a2
Toan Nguyen <vantoan1517@gmail.com>
2024-07-20 17:42:23 +0700

    Update users list page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "./user";
+import {UserService} from "./user.service";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {CdkTextareaAutosize} from "@angular/cdk/text-field";
 
 @Component({
-  selector: 'app-users',
-  standalone: true,
-  imports: [],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+    selector: 'app-users',
+    standalone: true,
+    imports: [
+        FaIconComponent,
+        CdkTextareaAutosize,
+    ],
+    templateUrl: './users.component.html',
+    styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+    }
+
+    ngOnInit(): void {
+        this.getAll();
+    }
+
+    getAll() {
+        this.userService.fetchAll().subscribe((users) => {
+            this.users = users;
+        });
+    }
+
+    protected readonly faSearch = faMagnifyingGlass;
 }
