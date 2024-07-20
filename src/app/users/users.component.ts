3a5a76d1935396a4082bfb56f78d29cac9d19610
Toan Nguyen <vantoan1517@gmail.com>
2024-07-20 19:03:01 +0700

    Update users search

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,6 +6,8 @@ import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {CdkTextareaAutosize} from "@angular/cdk/text-field";
 import {EditableDirective} from "../editable.directive";
 import {SorterComponent} from "../sorter/sorter.component";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {debounceTime, distinctUntilChanged, of, switchMap} from "rxjs";
 
 @Component({
     selector: 'app-users',
@@ -15,25 +17,53 @@ import {SorterComponent} from "../sorter/sorter.component";
         CdkTextareaAutosize,
         EditableDirective,
         SorterComponent,
+        ReactiveFormsModule,
     ],
     templateUrl: './users.component.html',
     styleUrl: './users.component.scss'
 })
 export class UsersComponent implements OnInit {
 
+    keyword = new FormControl('');
+
     users: User[] = [];
+
+    filteredUsers: User[] = [];
+
+    isFetching: boolean = false;
 
     constructor(private userService: UserService) {
     }
 
     ngOnInit(): void {
         this.getAll();
+
+        this.keyword.valueChanges.pipe(
+            debounceTime(100),
+            distinctUntilChanged(),
+            switchMap((term) => {
+                return this.filterItem(term);
+            })
+        ).subscribe(filteredUsers => {
+            this.filteredUsers = filteredUsers;
+        });
     }
 
     getAll() {
         this.userService.fetchAll().subscribe((users) => {
             this.users = users;
+            this.filteredUsers = users;
         });
+    }
+
+    filterItem(term: string | null) {
+        if (!term) {
+            return of(this.users)
+        }
+        const filteredUsers = this.users.filter(user =>
+            user.firstName?.toLowerCase().includes(term.toLowerCase())
+        );
+        return of(filteredUsers);
     }
 
     protected readonly faSearch = faMagnifyingGlass;
