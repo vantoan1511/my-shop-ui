26696e046ae7a330d777aef43a0f5ffb75df6558
Toan Nguyen <vantoan1517@gmail.com>
2024-10-17 00:43:23 +0700

    Added component

---
+++ src/app/shared/components/card-loader/card-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-loader.component.html',
+  styleUrl: './card-loader.component.scss'
+})
+export class CardLoaderComponent {
+
+}
