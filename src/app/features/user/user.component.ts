a39b7923e3a207d7745c5a5650f7482f4cd43f10
toan.nguyen <vantoan1517@gmail.com>
2024-10-31 00:58:34 +0700

    Add my order ui

--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -13,258 +13,301 @@ import {environment} from "../../../environments/environment";
 import {PageRequest} from "../../types/page-request.type";
 import {Image} from "../../types/image.type";
 import {PagedResponse} from "../../types/response.type";
+import {CurrencyPipe, DatePipe} from "@angular/common";
+import {Order} from "../../types/createOrderRequest";
 
 @Component({
-    selector: 'app-user',
-    standalone: true,
-    imports: [RouterLink, RouterLinkActive, RouterOutlet, ReactiveFormsModule, TranslateModule],
-    templateUrl: './user.component.html',
-    styleUrl: './user.component.scss',
+  selector: 'app-user',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, ReactiveFormsModule, TranslateModule, DatePipe, CurrencyPipe],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
 })
 export class UserComponent implements OnInit, OnDestroy {
-    userForm!: FormGroup;
-    passwordForm!: FormGroup;
-    username!: string;
-    userId: number | null = null;
-    avatarUrl: string = constant.defaultAvatar;
-    pageRequest: PageRequest = {page: 1, size: 10}
-    uploaded: PagedResponse<Image> | null = null;
-    private unsubscribe$ = new Subject<void>();
+  userForm!: FormGroup;
+  passwordForm!: FormGroup;
+  username!: string;
+  userId: number | null = null;
+  avatarUrl: string = constant.defaultAvatar;
+  pageRequest: PageRequest = {page: 1, size: 10}
+  uploaded: PagedResponse<Image> | null = null;
+  private unsubscribe$ = new Subject<void>();
 
-    constructor(
-        private translate: TranslateService,
-        private fb: FormBuilder,
-        private userService: UserService,
-        private alertService: AlertService,
-        private route: ActivatedRoute,
-        private authService: AuthenticationService,
-        private validator: ValidationService,
-        private imageService: ImageService,
-    ) {
-        this.translate.setDefaultLang("vi");
-    }
+  orders: Order[] = [
+    {
+      id: 123456,
+      createdAt: new Date().toString(),
+      modifiedAt: new Date().toString(),
+      orderStatus: 'COMPLETED',
+      totalAmount: 150.0,
+      paymentMethod: 'BANKING',
+      username: 'test',
+      shippingAddress: 'ABC 123'
+    },
+    {
+      id: 123456,
+      createdAt: new Date().toString(),
+      modifiedAt: new Date().toString(),
+      orderStatus: 'DECLINED',
+      totalAmount: 150.0,
+      paymentMethod: 'CASH',
+      username: 'test',
+      shippingAddress: 'ABC 123'
+    },
+  ];
+  selectedOrder: Order | null = null;
 
-    ngOnInit(): void {
-        this.username = this.getUsername();
-        this.userForm = this.initUserProfileForm();
-        this.passwordForm = this.initPasswordForm();
-        this.getUserProfile();
-    }
+  constructor(
+    private translate: TranslateService,
+    private fb: FormBuilder,
+    private userService: UserService,
+    private alertService: AlertService,
+    private route: ActivatedRoute,
+    private authService: AuthenticationService,
+    private validator: ValidationService,
+    private imageService: ImageService,
+  ) {
+    this.translate.setDefaultLang("vi");
+  }
 
-    private getUsername() {
-        const username = this.route.snapshot.paramMap.get('username');
-        return username ?? this.authService.username;
-    }
+  ngOnInit(): void {
+    this.username = this.getUsername();
+    this.userForm = this.initUserProfileForm();
+    this.passwordForm = this.initPasswordForm();
+    this.getUserProfile();
+  }
 
-    private initUserProfileForm() {
-        return this.fb.group({
-            username: [{value: '', disabled: true}],
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]],
-            phone: [
-                '',
-                [
-                    Validators.required,
-                    Validators.minLength(10),
-                    Validators.maxLength(10),
-                ],
-            ],
-            address: ['', Validators.required],
-            address1: [''],
-            address2: [''],
-            address3: [''],
-            address4: [''],
-            gender: [{value: 'UNKNOWN'}],
-        });
-    }
+  viewOrderDetails(order: Order) {
+    this.selectedOrder = order;
+  }
 
-    private initPasswordForm() {
-        const passwordForm = this.fb.group({
-            password: ['', Validators.required],
-            rePassword: ['', Validators.required],
-        });
-        passwordForm.addValidators(this.validator.passwordMatchValidator);
-        return passwordForm;
-    }
+  trackOrder(order: Order | null) {
+    // Add tracking logic here
+  }
 
-    private getUserProfile() {
-        const username = this.getUsername();
+  downloadInvoice(order: Order | null) {
+    // Add invoice download logic here
+  }
 
-        this.userService
-            .getByUsername(username)
-            .pipe(
-                takeUntil(this.unsubscribe$),
-                catchError((error) => {
-                    this.alertService.showErrorToast('Failed to get user: ' + username);
-                    return [];
-                }),
-                switchMap((user) => {
-                    this.userForm.patchValue(user);
-                    this.userId = user.id;
-                    this.avatarUrl = this.createAvatarUrl(this.userId);
-                    return this.imageService.getUploaded(this.pageRequest);
-                })
-            )
-            .subscribe((response) => this.uploaded = response);
-    }
+  private getUsername() {
+    const username = this.route.snapshot.paramMap.get('username');
+    return username ?? this.authService.username;
+  }
 
-    protected onProfileFormSubmit() {
-        if (this.userForm.valid) {
-            this.userService
-                .updateProfile(this.username, this.userForm.value)
-                .pipe(takeUntil(this.unsubscribe$))
-                .subscribe({
-                    next: () => {
-                        this.alertService.showSuccessToast('Profile updated successfully');
-                    },
-                    error: (error) => {
-                        this.alertService.showErrorToast('Failed to update profile');
-                    },
-                });
-        }
-    }
+  private initUserProfileForm() {
+    return this.fb.group({
+      username: [{value: '', disabled: true}],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(10),
+        ],
+      ],
+      address: ['', Validators.required],
+      address1: [''],
+      address2: [''],
+      address3: [''],
+      address4: [''],
+      gender: [{value: 'UNKNOWN'}],
+    });
+  }
 
-    protected onPasswordFormSubmit() {
-        if (this.passwordForm.valid) {
-            this.userService
-                .changePassword(this.username, this.passwordForm.get('password')?.value)
-                .pipe(takeUntil(this.unsubscribe$))
-                .subscribe({
-                    next: () =>
-                        this.alertService.showSuccessToast('Password changed successfully'),
-                    error: () =>
-                        this.alertService.showErrorToast('Failed to change password'),
-                });
-        }
-    }
+  private initPasswordForm() {
+    const passwordForm = this.fb.group({
+      password: ['', Validators.required],
+      rePassword: ['', Validators.required],
+    });
+    passwordForm.addValidators(this.validator.passwordMatchValidator);
+    return passwordForm;
+  }
 
-    protected previewImage(event: Event): void {
-        const input = event.target as HTMLInputElement;
-        if (!input.files?.[0]) {
-            return;
-        }
+  private getUserProfile() {
+    const username = this.getUsername();
 
-        const file = input.files[0];
-
-        if (!this.validateFile(file)) {
-            return;
-        }
-
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const imgElement = document.getElementById(
-                'avatar-preview'
-            ) as HTMLImageElement;
-            imgElement.src = e.target?.result as string;
-        };
-        reader.readAsDataURL(file);
-
-        this.imageService
-            .uploadImage(file)
-            .pipe(
-                takeUntil(this.unsubscribe$),
-                switchMap((resp) => {
-                    const avatarId = resp.id;
-                    return this.imageService.setAvatar(avatarId);
-                }),
-                catchError((error) => {
-                    this.alertService.showErrorToast('Failed to upload image');
-                    return [];
-                })
-            )
-            .subscribe({
-                next: () => {
-                    this.alertService.showSuccessToast('Updated avatar successfully')
-                },
-                error: () =>
-                    this.alertService.showErrorToast('Failed to update avatar'),
-            });
-    }
-
-    get isValidProfileForm() {
-        return this.userForm.valid;
-    }
-
-    get dirtyProfileForm() {
-        return this.userForm.dirty;
-    }
-
-    get notMatchedPassword() {
-        return this.passwordForm.errors?.['mismatch'];
-    }
-
-    private createAvatarUrl(userId: number) {
-        return `${environment.IMAGE_SERVICE_API}/images/avatar/users/${userId}`;
-    }
-
-    private validateFile(file: File) {
-        if (!file) {
-            this.alertService.showErrorToast('File is empty');
-            return false;
-        }
-
-        if (!this.validFileSize(file)) {
-            this.alertService.showErrorToast('File exceeds limit of 1MB');
-            return false;
-        }
-
-        if (!this.validFileType(file)) {
-            this.alertService.showErrorToast(
-                'Invalid file type. Only JPEG and PNG allowed'
-            );
-            return false;
-        }
-
-        return true;
-    }
-
-    private validFileSize(file: File) {
-        const MAX_FILE_SIZE = 1048576;
-        return file.size <= MAX_FILE_SIZE;
-    }
-
-    private validFileType(file: File) {
-        const validTypes = ['image/jpeg', 'image/png'];
-        return validTypes.includes(file.type);
-    }
-
-    protected createImageUrl(imageId: number) {
-        return `${environment.IMAGE_SERVICE_API}/images/${imageId}`;
-    }
-
-    protected onScrollGallery(event: Event) {
-        if (this.isScrolledToEnd(event) && this.uploaded?.hasNext) {
-            this.pageRequest = {...this.pageRequest, page: this.pageRequest.page + 1}
-            this.fetchImages()
-        }
-    }
-
-    protected setAvatar(image: Image) {
-        this.imageService.setAvatar(image.id).subscribe({
-            next: () => {
-                this.alertService.showSuccessToast('Avatar changed successfully')
-            },
+    this.userService
+      .getByUsername(username)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        catchError((error) => {
+          this.alertService.showErrorToast('Failed to get user: ' + username);
+          return [];
+        }),
+        switchMap((user) => {
+          this.userForm.patchValue(user);
+          this.userId = user.id;
+          this.avatarUrl = this.createAvatarUrl(this.userId);
+          return this.imageService.getUploaded(this.pageRequest);
         })
-    }
+      )
+      .subscribe((response) => this.uploaded = response);
+  }
 
-    private isScrolledToEnd(event: Event) {
-        const element = event.target as HTMLElement;
-        return element.scrollHeight - element.scrollTop === element.clientHeight;
-    }
-
-    private fetchImages() {
-        this.imageService.getUploaded(this.pageRequest).subscribe({
-            next: (response) => {
-                this.uploaded = {...response, items: [...(this.uploaded?.items || []), ...response.items]};
-            }
+  protected onProfileFormSubmit() {
+    if (this.userForm.valid) {
+      this.userService
+        .updateProfile(this.username, this.userForm.value)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: () => {
+            this.alertService.showSuccessToast('Profile updated successfully');
+          },
+          error: (error) => {
+            this.alertService.showErrorToast('Failed to update profile');
+          },
         });
     }
+  }
 
-    ngOnDestroy(): void {
-        this.unsubscribe$.next();
-        this.unsubscribe$.complete();
+  protected onPasswordFormSubmit() {
+    if (this.passwordForm.valid) {
+      this.userService
+        .changePassword(this.username, this.passwordForm.get('password')?.value)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: () =>
+            this.alertService.showSuccessToast('Password changed successfully'),
+          error: () =>
+            this.alertService.showErrorToast('Failed to change password'),
+        });
+    }
+  }
+
+  protected previewImage(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (!input.files?.[0]) {
+      return;
     }
 
-    protected readonly constant = constant;
+    const file = input.files[0];
+
+    if (!this.validateFile(file)) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const imgElement = document.getElementById(
+        'avatar-preview'
+      ) as HTMLImageElement;
+      imgElement.src = e.target?.result as string;
+    };
+    reader.readAsDataURL(file);
+
+    this.imageService
+      .uploadImage(file)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        switchMap((resp) => {
+          const avatarId = resp.id;
+          return this.imageService.setAvatar(avatarId);
+        }),
+        catchError((error) => {
+          this.alertService.showErrorToast('Failed to upload image');
+          return [];
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.alertService.showSuccessToast('Updated avatar successfully')
+        },
+        error: () =>
+          this.alertService.showErrorToast('Failed to update avatar'),
+      });
+  }
+
+  get isValidProfileForm() {
+    return this.userForm.valid;
+  }
+
+  get dirtyProfileForm() {
+    return this.userForm.dirty;
+  }
+
+  get notMatchedPassword() {
+    return this.passwordForm.errors?.['mismatch'];
+  }
+
+  private createAvatarUrl(userId: number) {
+    return `${environment.IMAGE_SERVICE_API}/images/avatar/users/${userId}`;
+  }
+
+  private validateFile(file: File) {
+    if (!file) {
+      this.alertService.showErrorToast('File is empty');
+      return false;
+    }
+
+    if (!this.validFileSize(file)) {
+      this.alertService.showErrorToast('File exceeds limit of 1MB');
+      return false;
+    }
+
+    if (!this.validFileType(file)) {
+      this.alertService.showErrorToast(
+        'Invalid file type. Only JPEG and PNG allowed'
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  private validFileSize(file: File) {
+    const MAX_FILE_SIZE = 1048576;
+    return file.size <= MAX_FILE_SIZE;
+  }
+
+  private validFileType(file: File) {
+    const validTypes = ['image/jpeg', 'image/png'];
+    return validTypes.includes(file.type);
+  }
+
+  protected createImageUrl(imageId: number) {
+    return `${environment.IMAGE_SERVICE_API}/images/${imageId}`;
+  }
+
+  protected onScrollGallery(event: Event) {
+    if (this.isScrolledToEnd(event) && this.uploaded?.hasNext) {
+      this.pageRequest = {...this.pageRequest, page: this.pageRequest.page + 1}
+      this.fetchImages()
+    }
+  }
+
+  protected setAvatar(image: Image) {
+    this.imageService.setAvatar(image.id).subscribe({
+      next: () => {
+        this.avatarUrl = this.imageUrlFromId(image.id);
+        this.alertService.showSuccessToast('Avatar changed successfully')
+      },
+    })
+  }
+
+  private isScrolledToEnd(event: Event) {
+    const element = event.target as HTMLElement;
+    return element.scrollHeight - element.scrollTop === element.clientHeight;
+  }
+
+  private fetchImages() {
+    this.imageService.getUploaded(this.pageRequest).subscribe({
+      next: (response) => {
+        this.uploaded = {...response, items: [...(this.uploaded?.items || []), ...response.items]};
+      }
+    });
+  }
+
+  private imageUrlFromId(imageId: number): string {
+    return `${environment.IMAGE_SERVICE_API}/images/${imageId}`;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  protected readonly constant = constant;
 }
