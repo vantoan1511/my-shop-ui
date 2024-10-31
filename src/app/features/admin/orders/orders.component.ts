f331a78166ba4d6526685f1a521d501cdd30396b
toan.nguyen <vantoan1517@gmail.com>
2024-11-01 01:46:33 +0700

    Add skeleton loader - order management

--- src/app/features/admin/orders/orders.component.ts
+++ src/app/features/admin/orders/orders.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {DataTableFooterComponent} from "../../../shared/components/pagination/pagination.component";
+import {PagedResponse} from "../../../types/response.type";
+import {Order} from "../../../types/order.type";
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    DataTableFooterComponent
+  ],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
 export class OrdersComponent {
 
+  loading = true
+  orderResponse: PagedResponse<Order> | null = null;
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang("vi");
+  }
+
+  onPageChange(pageChange: 'next' | 'previous') {
+
+  }
+
+  onPageSizeChange(sizeChange: number) {
+  }
+
+  protected readonly Array = Array;
 }
