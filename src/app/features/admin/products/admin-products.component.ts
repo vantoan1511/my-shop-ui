0f8c6599bdedf9ef6b2d339a17611b65fa97a8c7
toan.nguyen <vantoan1517@gmail.com>
2024-11-03 00:59:30 +0700

    Update thumbnail images

--- src/app/features/admin/products/admin-products.component.ts
+++ src/app/features/admin/products/admin-products.component.ts
@@ -12,164 +12,201 @@ import {ProductService} from "../../../services/product.service";
 import {AlertService} from "../../../services/alert.service";
 import {HttpErrorResponse} from "@angular/common/http";
 import {TranslateModule} from "@ngx-translate/core";
+import {environment} from "../../../../environments/environment";
+import {forkJoin, map, switchMap} from "rxjs";
+import {ProductImage} from "../../../types/image.type";
+import {constant} from "../../../shared/constant";
 
 @Component({
-    selector: 'admin-products',
-    standalone: true,
-    imports: [
-        DataTableFooterComponent,
-        DatePipe,
-        ListControlsComponent,
-        SortableDirective,
-        RouterLink,
-        CurrencyPipe,
-        NgClass,
-        TranslateModule
-    ],
-    templateUrl: './admin-products.component.html',
-    styleUrl: './admin-products.component.scss'
+  selector: 'admin-products',
+  standalone: true,
+  imports: [
+    DataTableFooterComponent,
+    DatePipe,
+    ListControlsComponent,
+    SortableDirective,
+    RouterLink,
+    CurrencyPipe,
+    NgClass,
+    TranslateModule
+  ],
+  templateUrl: './admin-products.component.html',
+  styleUrl: './admin-products.component.scss'
 })
 export class AdminProductsComponent implements OnInit {
-    loading = false;
-    pageRequest: PageRequest = {page: 1, size: 10};
-    sort: Sort = {sortBy: SortField.CREATED_AT, ascending: false};
-    products: PagedResponse<Product> | null = null
-    sortableFields = SortField;
-    selectAllChecked = false;
+  loading = false;
+  pageRequest: PageRequest = {page: 1, size: 10};
+  sort: Sort = {sortBy: SortField.CREATED_AT, ascending: false};
+  productResponse: PagedResponse<Product> | null = null
+  products: Product[] = [];
+  productImage: Map<number, string> = new Map()
+  sortableFields = SortField;
+  selectAllChecked = false;
 
-    constructor(
-        private productService: ProductService,
-        private alertService: AlertService
-    ) {
+  constructor(
+    private productService: ProductService,
+    private alertService: AlertService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  onRefreshButtonClick() {
+    this.fetchProducts();
+  }
+
+  onDeleteSelected() {
+    console.log('INFO - Starting deleting product...');
+    const selectedUsers = this.getSelectedProductIds();
+    const title = `Delete ${selectedUsers.length} product(s)?`;
+    const text =
+      'Those products will be deleted forever! Do you want to continue?';
+    this.alertService.showConfirmationAlert(title, text, 'warning', () =>
+      this.doDeleteSelectedProducts()
+    );
+  }
+
+  onPageChange(change: 'next' | 'previous') {
+    if (change === 'next') {
+      this.nextPage();
+    } else {
+      this.previousPage();
     }
+  }
 
-    ngOnInit(): void {
-        this.fetchProducts();
+  onPageSizeChange(size: number) {
+    this.pageRequest.size = size;
+    this.validatePageRequest();
+    this.fetchProducts();
+  }
+
+  onSelectAll(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    this.selectAllChecked = checkbox.checked;
+    const selectBoxes: NodeListOf<HTMLInputElement> =
+      document.querySelectorAll('.custom-checkbox');
+    selectBoxes.forEach((box) => {
+      box.checked = this.selectAllChecked;
+    });
+  }
+
+  onSortChange(sort: Sort) {
+    this.sort = sort;
+    this.fetchProducts();
+  }
+
+  onSelect() {
+    const selectBoxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(
+      '.custom-checkbox:not(#selectAll)'
+    );
+    this.selectAllChecked = Array.from(selectBoxes).every((box) => box.checked);
+
+    const selectAllCheckbox = document.querySelector(
+      '#selectAll'
+    ) as HTMLInputElement;
+    if (selectAllCheckbox) {
+      selectAllCheckbox.checked = this.selectAllChecked;
     }
+  }
 
-    onRefreshButtonClick() {
-        this.fetchProducts();
+  fetchProducts() {
+    this.loading = true
+    this.productService.getBy(this.pageRequest, this.sort).pipe(
+      map((productResponse) => {
+        this.productResponse = productResponse
+        this.products = productResponse.items
+        this.loading = false
+
+        return this.products.map(product => this.productService.getImagesById(product.id))
+      }),
+      switchMap((productImages) => forkJoin(productImages))
+    ).subscribe({
+      next: (response) => {
+        this.productImage = this.products.reduce((map, product, index) => {
+          const productImages = response.at(index) ?? []
+          map.set(product.id, this.pickThumbnailImage(productImages));
+          return map;
+        }, new Map<number, string>());
+        this.loading = false
+      },
+      error: () => this.loading = false
+    })
+  }
+
+  private nextPage() {
+    if (this.productResponse?.hasNext) {
+      this.pageRequest.page++;
+      this.fetchProducts();
     }
+  }
 
-    onDeleteSelected() {
-        console.log('INFO - Starting deleting product...');
-        const selectedUsers = this.getSelectedProductIds();
-        const title = `Delete ${selectedUsers.length} product(s)?`;
-        const text =
-            'Those products will be deleted forever! Do you want to continue?';
-        this.alertService.showConfirmationAlert(title, text, 'warning', () =>
-            this.doDeleteSelectedProducts()
+  private previousPage() {
+    if (this.productResponse?.hasPrevious) {
+      this.pageRequest.page--;
+      this.fetchProducts();
+    }
+  }
+
+  private validatePageRequest() {
+    if (this.productResponse) {
+      const itemsLeft =
+        this.productResponse.totalItems -
+        this.pageRequest.page * this.pageRequest.size;
+      if (itemsLeft < 0) {
+        this.pageRequest.page = Math.ceil(
+          this.productResponse.totalItems / this.pageRequest.size
         );
+      }
     }
+  }
 
-    onPageChange(change: 'next' | 'previous') {
-        if (change === 'next') {
-            this.nextPage();
-        } else {
-            this.previousPage();
-        }
-    }
+  private getSelectedProductIds(): number[] {
+    const selectBoxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(
+      '.custom-checkbox:not(#selectAll)'
+    );
+    return Array.from(selectBoxes)
+      .filter((box) => box.checked)
+      .map((box) => parseInt(box.id));
+  }
 
-    onPageSizeChange(size: number) {
-        this.pageRequest.size = size;
-        this.validatePageRequest();
+  private doDeleteSelectedProducts() {
+    this.productService.delete(this.getSelectedProductIds()).subscribe({
+      next: () => {
+        this.showSuccess();
         this.fetchProducts();
+      },
+      error: (error: HttpErrorResponse) => this.showError(error),
+    });
+  }
+
+  private showSuccess() {
+    const text = `${this.getSelectedProductIds().length} products deleted`;
+    this.alertService.showSuccessToast(text);
+  }
+
+  private showError(errorResponse: HttpErrorResponse) {
+    const {error} = errorResponse;
+    const errorMessage = error.errorMessage || 'Some server issues occurred';
+    this.alertService.showErrorToast(errorMessage);
+  }
+
+  private pickThumbnailImage(productImages: ProductImage[]) {
+    const featured = productImages.find(image => image.featured);
+    if (featured) {
+      return this.createImageUrl(featured.imageId);
     }
 
-    onSelectAll(event: Event) {
-        const checkbox = event.target as HTMLInputElement;
-        this.selectAllChecked = checkbox.checked;
-        const selectBoxes: NodeListOf<HTMLInputElement> =
-            document.querySelectorAll('.custom-checkbox');
-        selectBoxes.forEach((box) => {
-            box.checked = this.selectAllChecked;
-        });
-    }
+    const firstImage = productImages.at(0);
+    return firstImage ? this.createImageUrl(firstImage.imageId) : constant.defaultHeroImageUrl;
+  }
 
-    onSortChange(sort: Sort) {
-        this.sort = sort;
-        this.fetchProducts();
-    }
+  private createImageUrl(imageId: number) {
+    return `${environment.IMAGE_SERVICE_API}/images/${imageId}`
+  }
 
-    onSelect() {
-        const selectBoxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(
-            '.custom-checkbox:not(#selectAll)'
-        );
-        this.selectAllChecked = Array.from(selectBoxes).every((box) => box.checked);
 
-        const selectAllCheckbox = document.querySelector(
-            '#selectAll'
-        ) as HTMLInputElement;
-        if (selectAllCheckbox) {
-            selectAllCheckbox.checked = this.selectAllChecked;
-        }
-    }
-
-    private fetchProducts() {
-        this.loading = true
-        this.productService.getBy(this.pageRequest, this.sort).subscribe({
-            next: (response) => {
-                this.products = response
-                this.loading = false
-            },
-            error: () => this.loading = false
-        })
-    }
-
-    private nextPage() {
-        if (this.products?.hasNext) {
-            this.pageRequest.page++;
-            this.fetchProducts();
-        }
-    }
-
-    private previousPage() {
-        if (this.products?.hasPrevious) {
-            this.pageRequest.page--;
-            this.fetchProducts();
-        }
-    }
-
-    private validatePageRequest() {
-        if (this.products) {
-            const itemsLeft =
-                this.products.totalItems -
-                this.pageRequest.page * this.pageRequest.size;
-            if (itemsLeft < 0) {
-                this.pageRequest.page = Math.ceil(
-                    this.products.totalItems / this.pageRequest.size
-                );
-            }
-        }
-    }
-
-    private getSelectedProductIds(): number[] {
-        const selectBoxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(
-            '.custom-checkbox:not(#selectAll)'
-        );
-        return Array.from(selectBoxes)
-            .filter((box) => box.checked)
-            .map((box) => parseInt(box.id));
-    }
-
-    private doDeleteSelectedProducts() {
-        this.productService.delete(this.getSelectedProductIds()).subscribe({
-            next: () => {
-                this.showSuccess();
-                this.fetchProducts();
-            },
-            error: (error: HttpErrorResponse) => this.showError(error),
-        });
-    }
-
-    private showSuccess() {
-        const text = `${this.getSelectedProductIds().length} products deleted`;
-        this.alertService.showSuccessToast(text);
-    }
-
-    private showError(errorResponse: HttpErrorResponse) {
-        const {error} = errorResponse;
-        const errorMessage = error.errorMessage || 'Some server issues occurred';
-        this.alertService.showErrorToast(errorMessage);
-    }
+  protected readonly Array = Array;
+  protected readonly constant = constant;
 }
