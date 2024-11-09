b3c9d5e3c0c12ac88dcfa9c85a83a9b80361fb90
toan.nguyen <vantoan1517@gmail.com>
2024-11-10 00:17:16 +0700

    Update cart count

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AuthenticationService} from '../../services/authentication.service';
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {constant} from "../../shared/constant";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-header',
@@ -11,13 +12,22 @@ import {constant} from "../../shared/constant";
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  totalCarts = 0;
 
   constructor(
     protected authService: AuthenticationService,
     private translate: TranslateService,
+    private cartService: CartService,
   ) {
     this.translate.setDefaultLang('vi');
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated) {
+      this.cartService.totalCarts$.subscribe(count => this.totalCarts = count);
+    }
   }
 
   protected readonly constant = constant;
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,39 +1,54 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
-import {AddToCartRequest, Cart, CartItem} from "../types/cart.type";
+import {AddToCartRequest, Cart} from "../types/cart.type";
 import {PageRequest} from "../types/page-request.type";
 import {PagedResponse} from "../types/response.type";
+import {BehaviorSubject, tap} from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class CartService {
 
-    protected BASE_URL = environment.CART_SERVICE_API;
+  private totalCartsSubject = new BehaviorSubject<number>(0);
+  totalCarts$ = this.totalCartsSubject.asObservable();
 
-    constructor(private http: HttpClient) {
-    }
+  protected BASE_URL = environment.CART_SERVICE_API;
 
-    getCart(pageRequest: PageRequest) {
-        return this.http.get<PagedResponse<Cart>>(`${this.BASE_URL}/carts`, {
-            params: {...pageRequest}
-        })
-    }
+  constructor(private http: HttpClient) {
+    this.getCartCount()
+  }
 
-    addToCart(addToCartRequest: AddToCartRequest) {
-        return this.http.post<Cart>(`${this.BASE_URL}/carts/add`, addToCartRequest);
-    }
+  getCartCount() {
+    this.getCart({page: 1, size: 1}).subscribe(({totalItems}) => this.totalCartsSubject.next(totalItems));
+  }
 
-    removeCartItem(cartId: number) {
-        return this.http.delete(`${this.BASE_URL}/carts/${cartId}`);
-    }
+  getCart(pageRequest: PageRequest) {
+    return this.http.get<PagedResponse<Cart>>(`${this.BASE_URL}/carts`, {
+      params: {...pageRequest}
+    })
+  }
 
-    clearCart() {
-        return this.http.delete(`${this.BASE_URL}/carts`);
-    }
+  addToCart(addToCartRequest: AddToCartRequest) {
+    return this.http.post<Cart>(`${this.BASE_URL}/carts/add`, addToCartRequest).pipe(
+      tap(() => this.getCartCount())
+    );
+  }
 
-    updateQuantity(cartId: number, quantity: number) {
-        return this.http.put(`${this.BASE_URL}/carts/${cartId}`, {quantity});
-    }
+  removeCartItem(cartId: number) {
+    return this.http.delete(`${this.BASE_URL}/carts/${cartId}`).pipe(
+      tap(() => this.getCartCount())
+    );
+  }
+
+  clearCart() {
+    return this.http.delete(`${this.BASE_URL}/carts`).pipe(
+      tap(() => this.getCartCount())
+    );
+  }
+
+  updateQuantity(cartId: number, quantity: number) {
+    return this.http.put(`${this.BASE_URL}/carts/${cartId}`, {quantity});
+  }
 }
