81949320dc869e38c40e6c0aea9088d3ac072dd5
toan.nguyen <vantoan1517@gmail.com>
2024-11-12 00:49:29 +0700

    Update avatar display

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,9 @@ import {AuthenticationService} from '../../services/authentication.service';
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {constant} from "../../shared/constant";
 import {CartService} from "../../services/cart.service";
+import {UserService} from "../../services/user.service";
+import {User} from "../../types/user.type";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-header',
@@ -15,11 +18,13 @@ import {CartService} from "../../services/cart.service";
 export class HeaderComponent implements OnInit {
 
   totalCarts = 0;
+  user: User | null = null;
 
   constructor(
     protected authService: AuthenticationService,
     private translate: TranslateService,
     private cartService: CartService,
+    private userService: UserService,
   ) {
     this.translate.setDefaultLang('vi');
   }
@@ -27,7 +32,16 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     if (this.authService.isAuthenticated) {
       this.cartService.totalCarts$.subscribe(count => this.totalCarts = count);
+
+      this.userService.getByUsername(this.authService.username).subscribe(user => this.user = user);
     }
+  }
+
+  get avatarUrl() {
+    if (this.user && this.user.id) {
+      return `${environment.IMAGE_SERVICE_API}/images/avatar/users/${this.user.id}`;
+    }
+    return constant.defaultAvatar
   }
 
   protected readonly constant = constant;
