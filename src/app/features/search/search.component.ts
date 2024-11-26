c6fe0df4eec79c85cc95512c25dbca816438a4d1
toan.nguyen <vantoan1517@gmail.com>
2024-11-26 23:06:28 +0700

    Update to fetch product with featured image

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -8,12 +8,13 @@ import {Brand, Category, Product} from "../../types/product.type";
 import {BrandService} from "../../services/brand.service";
 import {CategoryService} from "../../services/category.service";
 import {ProductService} from "../../services/product.service";
-import {BehaviorSubject, debounceTime, distinctUntilChanged, switchMap} from "rxjs";
+import {BehaviorSubject, debounceTime, distinctUntilChanged, forkJoin, map, switchMap} from "rxjs";
 import {Sort, SortField} from "../../types/sort.type";
 import {PageRequest} from "../../types/page-request.type";
 import {FormsModule} from "@angular/forms";
 import {CardLoaderComponent} from "../../shared/components/card-loader/card-loader.component";
 import {ActivatedRoute, Router} from "@angular/router";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-search',
@@ -38,6 +39,8 @@ export class SearchComponent implements OnInit {
   categories: Category[] = []
   products: Product[] = []
 
+  featuredImageMap = new Map<number, string>();
+
   page = 1;
   size = 20;
   sortBy = SortField.CREATED_AT;
@@ -46,7 +49,6 @@ export class SearchComponent implements OnInit {
 
   keyword = ''
   keywordSubject = new BehaviorSubject<string>('');
-
 
   constructor(
     private brandService: BrandService,
@@ -89,18 +91,55 @@ export class SearchComponent implements OnInit {
   fetchProducts() {
     const pageRequest = {page: this.page, size: this.size} as PageRequest
     const sort = {sortBy: this.sortBy, ascending: this.ascending} as Sort
+
     this.productLoading = true
+
     this.keywordSubject.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((keyword) => this.productService.searchProducts(pageRequest, sort, {keyword}))
+      switchMap((keyword) =>
+        this.productService.searchProducts(pageRequest, sort, {keyword}).pipe(
+          switchMap((response) => {
+            const products = response.items;
+
+            const imageRequests = products.map((product) =>
+              this.productService.getImagesById(product.id).pipe(
+                map((images) => {
+                  const featuredImage = images.find((img) => img.featured) || images[0];
+                  const imageUrl = this.createImageUrl(featuredImage?.imageId)
+                  return {productId: product.id, imageUrl};
+                })
+              )
+            );
+
+            return forkJoin(imageRequests).pipe(
+              map((imageData) => {
+                const imageMap = new Map<number, string>();
+                imageData.forEach(({productId, imageUrl}) => {
+                  imageMap.set(productId, imageUrl);
+                });
+
+                return {
+                  pagedProduct: response,
+                  products,
+                  imageMap,
+                };
+              })
+            );
+          })
+        )
+      )
     ).subscribe({
-      next: (response) => {
-        this.pagedProduct = response
-        this.products = response.items;
+      next: ({pagedProduct, products, imageMap}) => {
+        this.pagedProduct = pagedProduct;
+        this.products = products;
+
+        this.featuredImageMap = imageMap;
         this.productLoading = false;
       },
-      error: () => this.productLoading = false
+      error: () => {
+        this.productLoading = false;
+      },
     })
   }
 
@@ -128,6 +167,17 @@ export class SearchComponent implements OnInit {
       queryParamsHandling: 'merge'
     });
     this.keywordSubject.next(keyword);
+  }
+
+  retrieveProductThumbnail(productId: number) {
+    return this.featuredImageMap.get(productId) || constant.defaultHeroImageUrl;
+  }
+
+  createImageUrl(imageId?: number) {
+    if (!imageId) {
+      return constant.defaultHeroImageUrl;
+    }
+    return `${environment.IMAGE_SERVICE_API}/images/${imageId}`;
   }
 
   protected readonly Array = Array;
